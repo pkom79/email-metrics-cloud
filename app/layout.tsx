4f3e0ee6fa235c82f295e4c20934bf1e5815ea3c
pkom79 <205697963+pkom79@users.noreply.github.com>
2025-11-09 13:33:26 -0500

    Add Meta Pixel tracking code to all pages
    Added Facebook Meta Pixel (ID: 1970128607177135) to the root layout head section to track PageView events across all pages of the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,28 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                 />
                 <link rel="icon" href="/icon.svg" type="image/svg+xml" />
                 <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+                {/* Meta Pixel Code */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1970128607177135');
+fbq('track', 'PageView');`
+                    }}
+                />
+                <noscript>
+                    <img height="1" width="1" style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=1970128607177135&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
+                {/* End Meta Pixel Code */}
             </head>
             <body className="min-h-screen bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
                 <AuthProvider>
